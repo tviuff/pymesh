54f456f703285b11fce2d7a7d5d55fcd072e3b5b
tviuff <tviuff@gmail.com>
2024-02-29 21:53:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Main module for trying out code ideas
+"""
+
+from mesh_generator import Point, Line
+
+point1 = Point(0.0, 0.0, 0.0)
+point2 = Point(1.0, 0.0, 0.0)
+point3 = Point(1.0, 1.0, 0.0)
+point4 = Point(0.0, 1.0, 0.0)
+
+for point in [point1, point2, point3, point4]:
+    print(point)
+
+line1 = Line(point1, point2)
+line2 = Line(point2, point3)
+line3 = Line(point3, point4)
+line4 = Line(point4, point1)
+
+for line in [line1, line2, line3, line4]:
+    print(line)
---
+++ mesh_generator/curves.py
@@ -0,0 +1,37 @@
+"""Module including curve and line classes
+"""
+
+import math
+import numpy as np
+
+class Line:
+    """Line generated from two points in space
+    """
+    def __init__(self, p0, p1):
+        assert (p0 != p1), f"Points P0 {p0} and P1 {p1} cannot be the same."
+        self.p0, self.p1 = p0, p1
+
+    def __eq__(self, other):
+        return self.p0 == other.p0 and self.p1 == other.p1
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.p0}, {self.p1})"
+
+    def get_points(self, n:int=10, method="linear"):
+        """Generates n internal points along the line using linear, cosine, cos01 and cos10 methods
+        """
+        assert (n > 1), f"N {n} is not larger than 1."
+        assert (method in ["linear", "cosine", "cos01", "cos10"]), \
+            f"method {method} not recognized."
+        xyz = np.zeros((n, 3))
+        for i, u in enumerate(np.linspace(0, 1, n, endpoint=True)):
+            if method == "cosine":
+                uu = math.cos(math.pi*(1 - u))/2 + 0.5
+            elif method == "cos01":
+                uu = math.cos(math.pi*u/2)
+            elif method == "cos10":
+                uu = 1-math.cos(math.pi*u/2)
+            else:
+                uu = u
+            xyz[i,:] = self.p0.xyz + (self.p1.xyz - self.p0.xyz)*uu
+        return xyz
