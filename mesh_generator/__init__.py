582b0113312fa0865d0928fa44da030cf0567b3f
Thomas Viuff <tviuff@gmail.com>
2024-02-29 22:58:21 +0100

    changed name of source code folder

--- mesh_generator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Module for initializing package
-"""
-
-from .points import Point
-from .curves import Line
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Module for initializing package
+"""
+
+from .points import Point
+from .curves import Line
