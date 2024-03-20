a35494ec12efbd8a353476d4090e90910266e49e
Thomas Viuff <tviuff@gmail.com>
2024-03-20 22:28:51 +0100

    removed .line_01 and .line_02 from PlaneSurface

--- gdf/surfaces/plane_surface.py
+++ gdf/surfaces/plane_surface.py
@@ -4,63 +4,64 @@
 import numpy as np
 from numpy import ndarray
 
-from gdf.curves import Line
 from gdf.constants import MeshConstants
 from gdf.points import Point
 from gdf.mesh.descriptors import BoundaryDistribution, MeshNumber
 from gdf.surfaces import Surface
 
-# ! Remove .line_01 and .line_02 from the class
+
+class SurfaceCornerPoint:
+    """Surface corner point descriptor class"""
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner) -> Point:
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, Point):
+            raise TypeError(f"{self._name} must be of type 'Point'")
+        instance.__dict__[self._name] = value
 
 class PlaneSurface(Surface):
     """Creates a plane surface based on three points in space
     and creates mesh points for generating panels.
     """
 
-    dist_01 = BoundaryDistribution()
-    dist_02 = BoundaryDistribution()
+    point0 = SurfaceCornerPoint()
+    point1 = SurfaceCornerPoint()
+    point2 = SurfaceCornerPoint()
+    dist01 = BoundaryDistribution()
+    dist02 = BoundaryDistribution()
     num_points_01 = MeshNumber()
     num_points_02 = MeshNumber()
 
     def __init__(self, point0:Point, point1:Point, point2:Point):
         self._all_surfaces.append(self)
-        self.line_01 = Line(point_start=point0, point_end=point1)
-        self.line_02 = Line(point_start=point0, point_end=point2)
-        self.dist_01 = MeshConstants.DEFAULT_DIST_METHOD.value
-        self.dist_02 = MeshConstants.DEFAULT_DIST_METHOD.value
+        self.point0 = point0
+        self.point1 = point1
+        self.point2 = point2
+        self.dist01 = MeshConstants.DEFAULT_DIST_METHOD.value
+        self.dist02 = MeshConstants.DEFAULT_DIST_METHOD.value
         self.num_points_01 = MeshConstants.DEFAULT_NUM_POINT.value
         self.num_points_02 = MeshConstants.DEFAULT_NUM_POINT.value
 
     @property
-    def line_01(self)-> Line:
-        return self._line_01
-
-    @line_01.setter
-    def line_01(self, value:Line) -> None:
-        if not isinstance(value, Line):
-            raise TypeError("line_01 must be of type 'Line'")
-        self._line_01 = value
-
-    @property
-    def line_02(self)-> Line:
-        return self._line_02
-
-    @line_02.setter
-    def line_02(self, value:Line) -> None:
-        if not isinstance(value, Line):
-            raise TypeError("line_02 must be of type 'Line'")
-        self._line_02 = value
-
-    @property
     def mesh_points(self) -> ndarray:
-        line1 = self.line_01.get_path_fn()
-        line2 = self.line_02.get_path_fn()
+
+        def line1(u):
+            fn = self.dist01.get_fn()
+            return self.point0.xyz + (self.point1.xyz - self.point0.xyz)*fn(u)
+
+        def line2(u):
+            fn = self.dist02.get_fn()
+            return self.point0.xyz + (self.point2.xyz - self.point0.xyz)*fn(u)
+
         np1 = self.num_points_01
         np2 = self.num_points_02
-        dist1 = self.dist_01.get_fn()
-        dist2 = self.dist_02.get_fn()
         mp = np.zeros((3, np1, np2))
         for i, u in enumerate(np.linspace(0, 1, num=np1, endpoint=True)):
             for j, w in enumerate(np.linspace(0, 1, num=np2, endpoint=True)):
-                mp[:, i, j] = line1(dist1(u)) + line2(dist2(w))
+                mp[:, i, j] = line1(u) + line2(w)
         return mp
