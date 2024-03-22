1b576f35c60addfb63e8561fa4fa2f3c035d36ef
Thomas Viuff <tviuff@gmail.com>
2024-03-22 08:02:46 +0100

    added examples/circle_quarter.py with a RuledSurface example

---
+++ examples/circle_quarter.py
@@ -0,0 +1,38 @@
+"""Circle panel example"""
+
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.join('..', 'gdf')))
+
+import math
+from gdf import Point, Line, Arc3P, PlaneSurface, RuledSurface, GDFViewer
+
+# Points forming an inner rectangle in the circle
+square_size = 0.8
+point00 = Point(0, 0, 0)
+point10 = Point(square_size/2, 0, 0)
+point01 = Point(0, square_size/2, 0)
+surface_rectangle = PlaneSurface(point00, point10, point01)
+
+# Points forming the outer surfaces of the circle
+circle_diameter = 2.0
+point11 = Point(square_size/2, square_size/2, 0)
+point11c = Point(circle_diameter/2/math.sqrt(2), circle_diameter/2/math.sqrt(2), 0)
+point10c = Point(circle_diameter/2, 0, 0)
+point01c = Point(0, circle_diameter/2, 0)
+
+# Outer surfaces of circle
+line10 = Line(point10, point11)
+arc10 = Arc3P(point00, point10c, point11c)
+surface_circle10 = RuledSurface(line10, arc10)
+surface_circle10.flip_normal = True
+line01 = Line(point01, point11)
+arc01 = Arc3P(point00, point01c, point11c)
+surface_circle01 = RuledSurface(line01, arc01)
+
+surface_selection = RuledSurface.get_all_surfaces()
+
+if __name__ == "__main__":
+    viewer = GDFViewer(panel_normal_length=0.5)
+    viewer.add_panels(surfaces=surface_selection, include_normals=True)
+    viewer.show()
